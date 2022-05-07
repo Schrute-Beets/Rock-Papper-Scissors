cc21b5314a871e154bfe1112e07bdbff12ab4634
Edgar Garcia <EdgarEGarcia3@gmail.com>
2022-05-06 21:15:42 -0500

    Create playRound function in script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-    let options = ["Rock", "Papper", "Scissors"];
+    let options = ["rock", "papper", "scissors"];
     let randomNumber = Math.floor(Math.random()*options.length);
     let computerSelection = options(randomNumber);
     return computerSelection;
@@ -9,7 +9,6 @@ function userPlay() {
     let validInput = false;
 
     while (!validInput) {
-        
         let input = prompt("Choose 'rock,' 'paper,' or 'scissors'!").toLowerCase;
         if (input === "rock" || input === "paper" || input === "scissors") {
             validInput = true;
@@ -17,6 +16,28 @@ function userPlay() {
         else {
             alert("Please enter a valid choice.");
         }
-        
     }
+
+    let userSelection = input;
+    return userSelection;
+}
+
+function playRound(computerSelection, userSelection) {
+    if (computerSelection === "rock" && userSelection === "rock"){}
+
+    else if (computerSelection === "rock" && userSelection === "paper"){}
+
+    else if (computerSelection === "rock" && userSelection === "scissors"){}
+
+    else if (computerSelection === "paper" && userSelection === "rock"){}
+
+    else if (computerSelection === "paper" && userSelection === "paper"){}
+
+    else if (computerSelection === "paper" && userSelection === "scissors"){}
+
+    else if (computerSelection === "scissors" && userSelection === "rock"){}
+
+    else if (computerSelection === "scissors" && userSelection === "paper"){}
+
+    else if (computerSelection === "scissors" && userSelection === "scissors"){}
 }
